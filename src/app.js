61f5bb6ae034e7fe4d292331e825fda3b6dfa590
chr1swill <alexanderwilliams20003@gmail.com>
2024-05-26 10:13:59 -0400

    refactor: add debug logger

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const DEBUG_MODE = true;
+
 (function () {
 	const DrawingAction = Object.freeze({
 		DRAW: "0",
@@ -301,12 +303,30 @@
 
 			this.#position = point;
 			this.#points.push(this.#position);
+			if (DEBUG_MODE === true) {
+				console.log("\n");
+				console.log("FROM: DrawingApp.#getPositions end");
+				console.log("\n");
+				console.log("event time : ", event.timeStamp);
+				console.log("event target", event.target);
+				console.log("pointer type: ", event.pointerType);
+				console.log("pointer id: ", event.pointerId);
+			}
 		}
 
 		/**
 		 * @param{TouchEvent} event
 		 */
 		#startDrawing(event) {
+			if (DEBUG_MODE === true) {
+				console.log("\n");
+				console.log("FROM: DrawingApp.#startDrawing start");
+				console.log("\n");
+				console.log("event time : ", event.timeStamp);
+				console.log("event target", event.target);
+				console.log("pointer type: ", event.pointerType);
+				console.log("pointer id: ", event.pointerId);
+			}
 			this.#isDrawing = true;
 			this.#getPostions(event);
 		}
